0038bb09edcac6830c028b3a221da60f7295bc6c
dandyxu <dandyjefferson@gmail.com>
2018-04-06 18:08:25 +0100

    Reorganize the header to diff whether on the front page or normal page

--- header.php
+++ header.php
@@ -32,57 +32,66 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-        <div class="nav-transparent">
+        <?php if ( is_front_page() ) : ?>
+
+            <div class="nav-transparent">
+
+                <nav class="navbar navbar-expand-md navbar-home">
+
+        <?php else : ?>
 
             <nav class="navbar navbar-expand-md navbar-light">
 
-            <?php if ( 'container' == $container ) : ?>
-                <div class="container" >
-            <?php endif; ?>
-
-                        <!-- Your site title as branding in the menu -->
-                        <?php if ( ! has_custom_logo() ) { ?>
-
-                            <?php if ( is_front_page() && is_home() ) : ?>
-
-                                <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-                            <?php else : ?>
-
-                                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-                            <?php endif; ?>
-
-
-                        <?php } else {
-                            the_custom_logo();
-                        } ?><!-- end custom logo -->
-
-
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <!-- The WordPress Menu goes here -->
-                    <?php wp_nav_menu(
-                        array(
-                            'theme_location'  => 'primary',
-                            'container_class' => 'collapse navbar-collapse justify-content-end',
-                            'container_id'    => 'navbarNavDropdown',
-                            'menu_class'      => 'navbar-nav',
-                            'fallback_cb'     => '',
-                            'menu_id'         => 'main-menu',
-                            'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-                        )
-                    ); ?>
-
+        <?php endif; ?>
 
 
                 <?php if ( 'container' == $container ) : ?>
-                </div><!-- .container -->
+                    <div class="container" >
                 <?php endif; ?>
 
-            </nav><!-- .site-navigation -->
+                            <!-- Your site title as branding in the menu -->
+                            <?php if ( ! has_custom_logo() ) { ?>
+
+                                <?php if ( is_front_page() && is_home() ) : ?>
+
+                                    <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+                                <?php else : ?>
+
+                                    <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+                                <?php endif; ?>
+
+
+                            <?php } else {
+                                the_custom_logo();
+                            } ?><!-- end custom logo -->
+
+
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <!-- The WordPress Menu goes here -->
+                        <?php wp_nav_menu(
+                            array(
+                                'theme_location'  => 'primary',
+                                'container_class' => 'collapse navbar-collapse justify-content-end',
+                                'container_id'    => 'navbarNavDropdown',
+                                'menu_class'      => 'navbar-nav',
+                                'fallback_cb'     => '',
+                                'menu_id'         => 'main-menu',
+                                'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+                            )
+                        ); ?>
+
+
+
+                    <?php if ( 'container' == $container ) : ?>
+                    </div><!-- .container -->
+                    <?php endif; ?>
+
+                </nav><!-- .site-navigation -->
 
             <div class="container">
                 <div class="row">
@@ -93,6 +102,11 @@ $container = get_theme_mod( 'understrap_container_type' );
                 </div>
 
             </div>
+
+    <?php if ( is_front_page() ) : ?>
         </div><!-- .nav-transparent -->
+
+    <?php endif; ?>
+
 	</div><!-- #wrapper-navbar end -->
 </div>
